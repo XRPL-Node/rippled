c33b0ae4638668d711c76e7a247795b48d028d2f
Mayukha Vadari <mvadari@gmail.com>
2025-11-25 02:58:57 +0530

    fix build issue

--- src/xrpld/app/wasm/detail/HostFuncImpl.cpp
+++ src/xrpld/app/wasm/detail/HostFuncImpl.cpp
@@ -424,7 +424,7 @@ WasmHostFunctionsImpl::checkSignature(
         return Unexpected(HostFunctionError::INVALID_PARAMS);
 
     PublicKey const pk(pubkey);
-    return verify(pk, message, signature, /*canonical*/ true);
+    return verify(pk, message, signature);
 }
 
 Expected<Hash, HostFunctionError>
