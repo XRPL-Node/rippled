5397bd6d6ede14266c20e302ff376dabeb17ac4c
yinyiqian1 <yqian@ripple.com>
2025-12-08 17:58:32 -0500

    fix naming

--- include/xrpl/protocol/ConfidentialTransfer.h
+++ include/xrpl/protocol/ConfidentialTransfer.h
@@ -17,8 +17,8 @@
 */
 //==============================================================================
 
-#ifndef RIPPLE_PROTOCOL_CONFIDENTIALTRANSFER_H_INCLUDED
-#define RIPPLE_PROTOCOL_CONFIDENTIALTRANSFER_H_INCLUDED
+#ifndef XRPL_PROTOCOL_CONFIDENTIALTRANSFER_H_INCLUDED
+#define XRPL_PROTOCOL_CONFIDENTIALTRANSFER_H_INCLUDED
 
 #include <xrpl/basics/Slice.h>
 #include <xrpl/protocol/Indexes.h>
