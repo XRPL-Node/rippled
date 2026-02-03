5dacfa1938bfb28d80e043999fa149ce0cc6f5e5
Pratik Mankawde <3397372+pratikmankawde@users.noreply.github.com>
2026-02-03 15:04:16 +0000

    clang-format updates
    Signed-off-by: Pratik Mankawde <3397372+pratikmankawde@users.noreply.github.com>

--- include/xrpl/basics/sanitizers.h
+++ include/xrpl/basics/sanitizers.h
@@ -1,7 +1,6 @@
 // Helper to disable ASan/HwASan for specific functions
 #if defined(__GNUC__) || defined(__clang__)
-#define XRPL_NO_SANITIZE_ADDRESS \
-    __attribute__((no_sanitize("address", "hwaddress")))
+#define XRPL_NO_SANITIZE_ADDRESS __attribute__((no_sanitize("address", "hwaddress")))
 #else
 #define XRPL_NO_SANITIZE_ADDRESS
 #endif
