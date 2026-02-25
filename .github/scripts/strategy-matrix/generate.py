5e25c1df93857513d888d4207ec5873a3d2ae718
Pratik Mankawde <3397372+pratikmankawde@users.noreply.github.com>
2026-02-25 17:29:10 +0000

    only build for clang for now
    Signed-off-by: Pratik Mankawde <3397372+pratikmankawde@users.noreply.github.com>

--- .github/scripts/strategy-matrix/generate.py
+++ .github/scripts/strategy-matrix/generate.py
@@ -236,12 +236,10 @@ def generate_strategy_matrix(all: bool, config: Config) -> list:
         # names get truncated.
         # Add Address and Thread (both coupled with UB) sanitizers for specific bookworm distros.
         # GCC-Asan rippled-embedded tests are failing because of https://github.com/google/sanitizers/issues/856
-        if os[
-            "distro_version"
-        ] == "bookworm" and f"{os['compiler_name']}-{os['compiler_version']}" in [
-            "clang-20",
-            "gcc-15",
-        ]:
+        if (
+            os["distro_version"] == "bookworm"
+            and f"{os['compiler_name']}-{os['compiler_version']}" == "clang-20"
+        ):
             # Add ASAN + UBSAN configuration.
             configurations.append(
                 {
